eecf6c1e6df021ddaef353b53e122ed161cf73d2
oldmc <oldmcdonnell@gmail.com>
2024-03-28 16:08:28 -0400

    js complete, adding css and html

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ let time = document.getElementById("time");
 let dateInput = document.getElementById("alarmDate");
 let tInput = document.getElementById("alarmTime");
 let btn = document.getElementById("setAlarm");
-let contan = document.getElementById("stAlarm");
+let contan = document.getElementById("alarms");
 let inerVal;
 let maxValue = 3;
 let cnt = 0;
@@ -22,14 +22,14 @@ function timeChangeFunction(){
         }
     }
     hrs = String(hrs).padStart(2, 0);
-    time.testContent = '${hrs}:${min}:{sec} ${period}';
+    time.textContent = '${hrs}:${min}:${sec} ${period}';
 }
 
 function alarmSetFunction() [
     let now = new Date();
     let selectedDate = new Date(dateInput.value + "T" + tInput.value);
     if (selectedDate <= now){
-        alert('invalid time. Please select a future time');
+        alert('invalid time. Please select a future date and time');
         return;
     }
 
@@ -47,9 +47,9 @@ if (cnt < maxValue) {
            $(selectedDate.tolocaleString())}
         </span>
         <button class= 'delete-alarm'>
-        delete
+        Delete
         </button>
-
+        ';
         alarmDiv
             .querySelector("delete-alarm")
             .addEventListener("click", () = >{
@@ -61,6 +61,42 @@ if (cnt < maxValue) {
                     almTimesArray.splice(idx, 1);
                 }
             });
+        interVal setTimeout(() => {
+            alert("Time to wake up!");
+            alarmDiv.remove();
+            cnt--;
+            const alarmIndex = almTimesArray.indexOf(selectedDate.toString());
+            if (alarmIndex !== -1) {
+                almTimesArray.splice(alarmIndex, 1);
+            }
+        }, timeUntilAlarm);
+        contan.appendChild(alarmDiv);
+        cnt++;
+        almTimesArray.push(selectedDate.toString());
+    } else {
+        alert("You can only set a maximum of 3 alarms")
+    }
+
+}
+function showAlarmFunction() {
+    let alarms = contan.querySelectorAll(".alarm");
+    alarms.forEach((alarm) => {
+        let deleteButton = alarm.querySelector(".delete-alarm");
+        deleteButton.addEventListener("click", () => {
+            alarmDiv.remove()
+            cnt--;
+            clearTimeout(interVal);
+            const alarmIndex = almTimesArray.indexOf(selectedDate.toString());
+            if (alarmIndex !== -1) {
+                almTimesArray.splice(alarmIndex, 1);
+            }
+        });
+    });
+}
+showAlarmFunction();
+setInterval(timeChangeFunction, 1000);
+btn.addEventListener("click", alarmSetFunction)
+timeChangeFunction();
 
 //const displayedClock = document.querySelector('.displayed-clock');
 
