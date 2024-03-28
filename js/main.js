455d118b4ab2692bde7785c4e6d7a86e6db842ad
oldmc <oldmcdonnell@gmail.com>
2024-03-28 05:52:21 -0400

    getting rid of doubt

--- js/main.js
+++ js/main.js
@@ -1,3 +1,35 @@
+let time = document.getElementById("time");
+let dateInput = document.getElementById("alarmDate");
+let tInput = document.getElementById("alarmTime");
+let btn = document.getElementById("setAlarm");
+let contan = document.getElementById("stAlarm");
+let inerVal;
+let maxValue = 3;
+let cnt = 0;
+let almTimesArray = [];
+//Timechange function
+
+function timeChangeFunction(){
+    let curr = new Date();
+    let hrs = curr.getHours();
+    let min = String(curr.getMinutes()).padStart(2, "0")
+    let sec = String(curr.getMinutes()).padStart(2, "0")
+    let period = "AM";
+    if (hrs >= 12) {
+        period = "PM";
+        if (hrs > 12){
+            hrs -= 12;
+        }
+    }
+    hrs = String(hrs).padStart(2, 0);
+    time.testContent = '${hrs}:${min}:{sec} ${period}';
+}
+
+function alarmSetFunction() [
+    let now = new Date();
+    let selectedDate = new Date(dateInput.value + "T" + tInput.value)
+]
+
 //const displayedClock = document.querySelector('.displayed-clock');
 
 function startTime() {
