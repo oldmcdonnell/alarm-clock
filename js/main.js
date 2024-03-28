bebf63b03b4d36e3ac2a4e3da6ef991e859635f8
oldmc <oldmcdonnell@gmail.com>
2024-03-28 16:50:55 -0400

    starting over

--- js/main.js
+++ js/main.js
@@ -3,55 +3,52 @@ let dateInput = document.getElementById("alarmDate");
 let tInput = document.getElementById("alarmTime");
 let btn = document.getElementById("setAlarm");
 let contan = document.getElementById("alarms");
-let inerVal;
+let interVal;
 let maxValue = 3;
 let cnt = 0;
 let almTimesArray = [];
-//Timechange function
-
-function timeChangeFunction(){
+function timeChangeFunction() {
     let curr = new Date();
     let hrs = curr.getHours();
-    let min = String(curr.getMinutes()).padStart(2, "0")
-    let sec = String(curr.getMinutes()).padStart(2, "0")
+    let min = String(curr.getMinutes()).padStart(2, "0");
+    let sec = String(curr.getSeconds()).padStart(2, "0");
     let period = "AM";
     if (hrs >= 12) {
         period = "PM";
-        if (hrs > 12){
+        if (hrs > 12) {
             hrs -= 12;
         }
     }
-    hrs = String(hrs).padStart(2, 0);
-    time.textContent = '${hrs}:${min}:${sec} ${period}';
+    hrs = String(hrs).padStart(2, "0");
+    time.textContent = `${hrs}:${min}:${sec} ${period}`;
 }
-
 function alarmSetFunction() {
     let now = new Date();
     let selectedDate = new Date(dateInput.value + "T" + tInput.value);
-    if (selectedDate <= now){
-        alert('invalid time. Please select a future date and time');
+    if (selectedDate <= now) {
+        alert(`Invalid time. Please select 
+      a future date and time.`);
         return;
     }
-
-if (almTimesArray.includes(selectedDate.toString())){
-    alert('You cannot set multiple alarms for the same time.')
-    return;
-}
-
-if (cnt < maxValue) {
-    let timeUntilAlarm = selectedDate - now;
-    let alarmDiv = document.createElement("div");
-    alarmDiv.classList.add("alarm");
-    alarmDiv.innerHTML = `
-        <span>
-          ${selectedDate.toLocaleString()}
-        </span>
-        <button class="delete-alarm">
-          Delete
-        </button>
-    `;
+    if (almTimesArray.includes(selectedDate.toString())) {
+        alert(`You cannot set multiple 
+      alarms for the same time.`);
+        return;
+    }
+    if (cnt < maxValue) {
+        let timeUntilAlarm = selectedDate - now;
+        let alarmDiv = document.createElement("div");
+        alarmDiv.classList.add("alarm");
+        alarmDiv.innerHTML = `
+            <span>
+              ${selectedDate.toLocaleString()}
+            </span>
+            <button class="delete-alarm">
+              Delete
+            </button>
+        `;
         alarmDiv
-            .querySelector("delete-alarm")
+            .querySelector(".delete-alarm")
             .addEventListener("click", () => {
                 alarmDiv.remove();
                 cnt--;
@@ -62,7 +59,7 @@ if (cnt < maxValue) {
                 }
             });
         interVal = setTimeout(() => {
-            alert("Wake the fuck up!");
+            alert("Time to wake up!");
             alarmDiv.remove();
             cnt--;
             const alarmIndex = almTimesArray.indexOf(selectedDate.toString());
@@ -74,9 +71,8 @@ if (cnt < maxValue) {
         cnt++;
         almTimesArray.push(selectedDate.toString());
     } else {
-        alert("You can only set a maximum of 3 alarms")
+        alert("You can only set a maximum of 3 alarms.");
     }
-
 }
 function showAlarmFunction() {
     let alarms = contan.querySelectorAll(".alarm");
@@ -95,11 +91,10 @@ function showAlarmFunction() {
 }
 showAlarmFunction();
 setInterval(timeChangeFunction, 1000);
-btn.addEventListener("click", alarmSetFunction)
+btn.addEventListener("click", alarmSetFunction);
 timeChangeFunction();
-
 //const displayedClock = document.querySelector('.displayed-clock');
-
+/*-
 function startTime() {
     const today = new Date();
     let h = today.getHours();
@@ -123,3 +118,4 @@ function startTime() {
   //listener for time?
   //when time = this time alarmTrigger
   //setAlarm, when current time = x 
+  */
