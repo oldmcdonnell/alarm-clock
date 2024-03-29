4b336670605e44c1bc54804609d8d92245e9f006
oldmc <oldmcdonnell@gmail.com>
2024-03-29 11:41:29 -0400

    finishing u

--- js/main.js
+++ js/main.js
@@ -7,21 +7,66 @@ let interVal;
 let maxValue = 3;
 let cnt = 0;
 let almTimesArray = [];
+
 function timeChangeFunction() {
     let curr = new Date();
     let hrs = curr.getHours();
     let min = String(curr.getMinutes()).padStart(2, "0");
     let sec = String(curr.getSeconds()).padStart(2, "0");
     let period = "AM";
+    var timeString = hrs + ":" + min + ":" + sec;
+    var img = stringToImage(timeString);
     if (hrs >= 12) {
         period = "PM";
         if (hrs > 12) {
             hrs -= 12;
         }
     }
+    
     hrs = String(hrs).padStart(2, "0");
     time.textContent = `${hrs}:${min}:${sec} ${period}`;
+    let imageClock = document.getElementById('clock').innerHTML = timeString + "<br>" + img;
+    var temp = ""
+    for (var i = 0; i < s.length; i++) {
+      //URL from array img and shove it into a HTML img tag
+      //also append it to the temporary variable
+      temp = temp + "<img src='" + img[s[i]] + "'/>"
+    }
+    //uncomment below line to see what the temporary variable says
+    console.log(temp)
+    return temp
 }
+
+  //This function takes each letter of an array and pairs it to an image of the img array 
+  function stringToImage(s) {
+    //create a temporary blank variable to hold HTML images
+    var temp = ""
+    for (var i = 0; i < s.length; i++) {
+      //URL from array img and shove it into a HTML img tag
+      //also append it to the temporary variable
+      temp = temp + "<img src='" + img[s[i]] + "'/>"
+    }
+    //uncomment below line to see what the temporary variable says
+    console.log(temp)
+    return temp
+  }
+  
+  
+  //all image URLs are put into this array. feel free to change URLs
+  var img = {
+    "1": "img/1.jpg",
+    "2": "img/2.jpg",
+    "3": "img/3.jpg",
+    "4": "img/4.jpg",
+    "5": "img/5.jpg",
+    "6": "img/6.jpg",
+    "7": "img/7.jpg",
+    "8": "img/8.jpg",
+    "9": "img/9.jpg",
+    "0": "img/0.jpg",
+    ":": "img/colon.jpg",
+  }
+
 function alarmSetFunction() {
     let now = new Date();
     let selectedDate = new Date(dateInput.value + "T" + tInput.value);
